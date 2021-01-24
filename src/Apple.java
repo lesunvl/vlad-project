1d8a007f3ad1913f1178d06fee18add0ccde68bc
Vlad Lesun <vladlesun@vlads-mbp.home>
2021-01-24 08:44:22 -0500

    third commit

--- src/Apple.java
+++ src/Apple.java
@@ -2,6 +2,6 @@ public class Apple {
     public static void main(String[] args) {
         System.out.println("Apple better than kiwi");
         System.out.println("Just do it");
-
+        System.out.println("I am loving it");
     }
 }
